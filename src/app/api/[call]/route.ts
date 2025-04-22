71487188f21921d5439805cb4504d77c32672f03
sirdeggen <d.kellenschwiler@bsvblockchain.org>
2025-04-22 16:13:50 -0500

    more logging
    Signed-off-by: sirdeggen <d.kellenschwiler@bsvblockchain.org>

--- src/app/api/[call]/route.ts
+++ src/app/api/[call]/route.ts
@@ -2,6 +2,7 @@ import { WalletStorageManager, Services, Wallet, StorageClient } from '@bsv/wall
 import { KeyDeriver, PrivateKey, WalletWireProcessor } from '@bsv/sdk'
 
 const createWallet = async () => {
+  console.log({ WALLET_STORAGE_URL: process.env.WALLET_STORAGE_URL, WALLET_ROOT_KEY_HEX: process.env.WALLET_ROOT_KEY_HEX })
   const endpointUrl = process.env.WALLET_STORAGE_URL!
   const rootKey = PrivateKey.fromHex(process.env.WALLET_ROOT_KEY_HEX!)
   const keyDeriver = new KeyDeriver(rootKey)
@@ -15,24 +16,30 @@ const createWallet = async () => {
     services,
   })
   const client = new StorageClient(wallet, endpointUrl)
-    await storage.addWalletStorageProvider(client)
-    await storage.makeAvailable()
-    return wallet
+  await storage.addWalletStorageProvider(client)
+  await storage.makeAvailable()
+  return wallet
 }
 
 export async function POST(req: Request) {
   try {
     const data = await req.arrayBuffer()
+    console.log({ data })
     const wallet = await createWallet()
-    console.log(await wallet.isAuthenticated({}))
+    const auth = await wallet.isAuthenticated({})
+    console.log({ auth })
 
     const w = new WalletWireProcessor(wallet)
 
     // convert the arrayBuffer to number[]
     const message = Array.from(new Uint8Array(data))
 
+    console.log({ message })
+
     // transmit the message to the wallet
     const result = await w.transmitToWallet(message)
+    
+    console.log({ result })
 
     // transmit the result as an arrayBuffer back to the client
     return new Response(Buffer.from(result), { headers: { 'Content-Type': 'application/octet-stream' } })
